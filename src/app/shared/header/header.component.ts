b1a104613faf5bcfdebf96278d8d253ee749f588
VelizarStavrev <velizar.g.stavrev@gmail.com>
2022-12-18 21:44:25 +0200

    Adding mobile responsiveness to the header component.

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -6,10 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  isVisible: boolean = false;
+  isChanged: boolean = false;
+  hamburgerClassList: string[] = ['hamburger-button'];
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  toggleVisibleClass() {
+    this.isVisible = !this.isVisible;
+
+    if (this.isVisible) {
+      this.hamburgerClassList = ['hamburger-button', 'active'];
+    }
+
+    if (this.isChanged && !this.isVisible) {
+      this.hamburgerClassList = ['hamburger-button', 'active-reverse'];
+    }
+
+    this.isChanged = true;
+  }
 }
