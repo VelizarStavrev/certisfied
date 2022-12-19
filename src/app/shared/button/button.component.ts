b7fc475cfd06a597b15c6ec7c21e7f97f66d1b3a
VelizarStavrev <velizar.g.stavrev@gmail.com>
2022-12-19 15:09:48 +0200

    Adding the button component.

---
+++ src/app/shared/button/button.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.scss']
+})
+export class ButtonComponent implements OnInit {
+  @Input() buttonText = 'Click'; // decorate the property with @Input()
+  @Input() buttonType = 'Primary'; // decorate the property with @Input()
+  @Input() buttonHTMLType = 'button'; // decorate the property with @Input()
+  @Input() buttonMarginTop = ''; // decorate the property with @Input()
+  @Input() buttonMarginBottom = ''; // decorate the property with @Input()
+  @Input() buttonMarginLeft = ''; // decorate the property with @Input()
+  @Input() buttonMarginRight = ''; // decorate the property with @Input()
+  @Input() buttonFunc = () => {}; // decorate the property with @Input()
+  buttonClasses = '';
+
+  constructor() { }
+
+  ngOnInit(): void {
+    switch (this.buttonType) {
+        case 'Primary':
+            this.buttonClasses = 'button-base button-primary';
+            break;
+
+        case 'Secondary':
+            this.buttonClasses = 'button-base button-secondary';
+            break;
+
+        case 'Error':
+            this.buttonClasses = 'button-base button-error';
+            break;
+
+        default:
+            this.buttonClasses = 'button-base button-primary';
+    }
+
+    if (this.buttonMarginTop) {
+        this.buttonClasses += ' button-margin-top';
+    }
+
+    if (this.buttonMarginBottom) {
+        this.buttonClasses += ' button-margin-bottom';
+    }
+
+    if (this.buttonMarginLeft) {
+        this.buttonClasses += ' button-margin-left';
+    }
+
+    if (this.buttonMarginRight) {
+        this.buttonClasses += ' button-margin-right';
+    }
+  }
+}
