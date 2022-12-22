bb17cda75085905261bd20e68f6663085cfd15e5
VelizarStavrev <velizar.g.stavrev@gmail.com>
2022-12-22 21:28:34 +0200

    Updating the register component to use the register user service.

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { UserService } from 'src/app/services/user.service';
+import { Register } from 'src/app/interfaces/register';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -14,7 +17,7 @@ export class RegisterComponent implements OnInit {
   buttonLink: string = '/login';
   buttonTypeLink: string = 'Secondary';
   buttonTextLink: string = 'Login';
-  
+
   registerForm = new FormGroup({
     email: new FormControl('', [Validators.required, Validators.email]),
     username: new FormControl('', [Validators.required, Validators.minLength(3)]),
@@ -26,16 +29,49 @@ export class RegisterComponent implements OnInit {
   get username() { return this.registerForm.get('username'); }
   get password() { return this.registerForm.get('password'); }
   get repassword() { return this.registerForm.get('repassword'); }
+  emailExistsError: boolean = false;
+  usernameExistsError: boolean = false;
   formError: boolean = false;
 
   onSubmit() {
-    // Remove the form error
+    // Remove the form errors
     this.formError = false;
+    this.emailExistsError = false;
+    this.usernameExistsError = false;
+
+    // TO DO - Start the loader
 
     // Send the form if it is valid
     if (this.registerForm.valid) {
-      console.log(this.registerForm.value);
-      // TO DO - send the request with a service and handle it on fail
+      this.userService.userRegister(this.registerForm.value)
+        .subscribe((data: Register) => {
+          if (data.status) {
+            // TO DO - add message
+            // TO DO - hide loader
+
+            this.router.navigate(['/login']);
+            return;
+          }
+
+          switch (data.type) {
+            case 'exists-email':
+              this.emailExistsError = true;
+              break;
+
+            case 'exists-username':
+              this.usernameExistsError = true;
+              break;
+
+            case 'error':
+            default:
+              this.formError = true;
+          }
+
+          this.formError = true;
+          console.log(this.emailExistsError);
+          // TO DO - add message
+          // TO DO - hide loader
+        });
       return;
     }
 
@@ -43,7 +79,7 @@ export class RegisterComponent implements OnInit {
     this.formError = true;
   }
 
-  constructor() { }
+  constructor(public userService: UserService, public router: Router) { }
 
   ngOnInit(): void {
   }
