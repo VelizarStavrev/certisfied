c6eb6fbce9d5ef77d23f3cc7ad988be60d1bf67e
VelizarStavrev <velizar.g.stavrev@gmail.com>
2022-12-24 17:22:24 +0200

    Implementing the loader service for the login component.

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -3,6 +3,7 @@ import { UserService } from '../../services/user.service';
 import { Login } from '../../interfaces/login';
 import { Router } from '@angular/router';
 import { MessageService } from 'src/app/services/message.service';
+import { LoaderService } from 'src/app/services/loader.service';
 
 @Component({
   selector: 'app-login',
@@ -26,14 +27,19 @@ export class LoginComponent implements OnInit {
   formErrorMessage = 'Form error';
 
   onSubmit() {
+    // Remove the form errors
     this.formError = false;
     
+    // Show the loader
+    this.loaderService.showLoader(true);
+
+    // Send the form data
     this.userService.userLogin(this.username, this.password)
       .subscribe((data: Login) => {
-
         if (!data.status) {
           this.formError = true;
           this.formErrorMessage = data.message;
+          this.loaderService.showLoader(false);
           return;
         }
 
@@ -44,10 +50,18 @@ export class LoginComponent implements OnInit {
 
         // Add a message for successful login
         this.messageService.setMessage({type: 'message-success', message: 'Successfully logged in.'});
+
+        // Hide the loader
+        this.loaderService.showLoader(false);
       });
   }
 
-  constructor(public userService: UserService, public router: Router, public messageService: MessageService) { }
+  constructor(
+    public userService: UserService, 
+    public router: Router, 
+    public messageService: MessageService, 
+    public loaderService: LoaderService
+  ) { }
 
   ngOnInit(): void {
   }
