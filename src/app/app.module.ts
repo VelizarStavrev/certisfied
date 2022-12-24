6b281735611177c0b4fa9638602e43767564db82
VelizarStavrev <velizar.g.stavrev@gmail.com>
2022-12-24 17:36:29 +0200

    Adding the initial profile component and implementing the user service logout functionality.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { ButtonLinkComponent } from './components/shared/button-link/button-link
 import { RegisterComponent } from './components/register/register.component';
 import { MessageComponent } from './components/shared/message/message.component';
 import { LoaderComponent } from './components/shared/loader/loader.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
 @NgModule({
   declarations: [
@@ -26,7 +27,8 @@ import { LoaderComponent } from './components/shared/loader/loader.component';
     ButtonLinkComponent,
     RegisterComponent,
     MessageComponent,
-    LoaderComponent
+    LoaderComponent,
+    ProfileComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/profile/profile.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/services/user.service';
+
+@Component({
+  selector: 'app-profile',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.scss']
+})
+export class ProfileComponent implements OnInit {
+  buttonText: string = 'Logout';
+  buttonType: string = 'Secondary';
+  buttonHTMLType: string = 'button';
+  
+  buttonFunc(): void {
+    this.userService.userLogout();
+  }
+
+  constructor(public userService: UserService) { }
+
+  ngOnInit(): void {
+  }
+
+}
