b31198dfddbc29712be5afed958e7b27cd644c7d
VelizarStavrev <velizar.g.stavrev@gmail.com>
2022-12-24 17:48:43 +0200

    Adding types to the button link component and minor adjustments.

--- src/app/components/shared/button-link/button-link.component.ts
+++ src/app/components/shared/button-link/button-link.component.ts
@@ -6,50 +6,50 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./button-link.component.scss']
 })
 export class ButtonLinkComponent implements OnInit {
-  @Input() buttonText = 'Click'; // decorate the property with @Input()
-  @Input() buttonType = 'Primary'; // decorate the property with @Input()
-  @Input() buttonLink = ''; // decorate the property with @Input()
-  @Input() buttonMarginTop = ''; // decorate the property with @Input()
-  @Input() buttonMarginBottom = ''; // decorate the property with @Input()
-  @Input() buttonMarginLeft = ''; // decorate the property with @Input()
-  @Input() buttonMarginRight = ''; // decorate the property with @Input()
+  @Input() buttonText: string = 'Click'; // decorate the property with @Input()
+  @Input() buttonType: string = 'Primary'; // decorate the property with @Input()
+  @Input() buttonLink: string = ''; // decorate the property with @Input()
+  @Input() buttonMarginTop: string = ''; // decorate the property with @Input()
+  @Input() buttonMarginBottom: string = ''; // decorate the property with @Input()
+  @Input() buttonMarginLeft: string = ''; // decorate the property with @Input()
+  @Input() buttonMarginRight: string = ''; // decorate the property with @Input()
   @Input() buttonFunc = () => { }; // decorate the property with @Input()
-  buttonClasses = '';
+  buttonClasses: string[] = [];
 
   constructor() { }
 
   ngOnInit(): void {
     switch (this.buttonType) {
       case 'Primary':
-        this.buttonClasses = 'link-button link-button-primary';
+        this.buttonClasses.push('link-button', 'link-button-primary');
         break;
 
       case 'Secondary':
-        this.buttonClasses = 'link-button link-button-secondary';
+        this.buttonClasses.push('link-button', 'link-button-secondary');
         break;
 
       case 'Link':
-        this.buttonClasses = 'link-plain';
+        this.buttonClasses.push('link-plain');
         break;
 
       default:
-        this.buttonClasses = 'link-plain';
+        this.buttonClasses.push('link-plain');
     }
 
     if (this.buttonMarginTop) {
-      this.buttonClasses += ' button-margin-top';
+      this.buttonClasses.push('button-margin-top');
     }
 
     if (this.buttonMarginBottom) {
-      this.buttonClasses += ' button-margin-bottom';
+      this.buttonClasses.push('button-margin-bottom');
     }
 
     if (this.buttonMarginLeft) {
-      this.buttonClasses += ' button-margin-left';
+      this.buttonClasses.push('button-margin-left');
     }
 
     if (this.buttonMarginRight) {
-      this.buttonClasses += ' button-margin-right';
+      this.buttonClasses.push('button-margin-right');
     }
   }
 }
