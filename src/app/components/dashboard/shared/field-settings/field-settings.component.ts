de5429d76b7224fd2cda656e464150955a1f8f2d
VelizarStavrev <velizar.g.stavrev@gmail.com>
2023-01-09 00:02:32 +0200

    Adding the field settings component.

---
+++ src/app/components/dashboard/shared/field-settings/field-settings.component.ts
@@ -0,0 +1,76 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Field } from 'src/app/interfaces/field';
+
+@Component({
+  selector: 'app-field-settings',
+  templateUrl: './field-settings.component.html',
+  styleUrls: ['./field-settings.component.scss']
+})
+export class FieldSettingsComponent implements OnInit {
+  buttonText: string = 'Done';
+  buttonType: string = 'Primary';
+  buttonHTMLType: string = 'button';
+  arrowIcon: string = '../../../../assets/icons/arrow.svg';
+  @Input() visibility: boolean = false;
+  @Input() currentFieldData: Field | null = null;
+  @Output() buttonDoneClick = new EventEmitter<{}>();
+  @Output() editField = new EventEmitter<{ fieldId: number, fieldPropertyName: string, fieldValue: string }>();
+  @Output() editFieldUnit = new EventEmitter<{ fieldId: number, fieldPropertyName: string, fieldUnit: string, fieldIndex: number }>();
+
+  hideFieldSettingsMenu() {
+    this.buttonDoneClick.emit();
+  }
+
+  updateField(fieldPropertyName: string, event: any, fieldPropertyValue: string = '', fieldIndex: number): void {
+    let fieldValue: string = fieldPropertyValue;
+
+    let fieldData: { fieldId: number, fieldPropertyName: string, fieldValue: string, fieldIndex: number } = {
+      fieldId: this.currentFieldData!.id,
+      fieldPropertyName,
+      fieldValue,
+      fieldIndex
+    };
+
+    if (this.currentFieldData?.type === 'Image' && fieldPropertyName === 'url') {
+      let files: FileList = event.target.files;
+      let fileReader = new FileReader();
+      fileReader.readAsDataURL(files[0]);
+
+      fileReader.onload = (event: any) => {
+        let imageData = event.target.result;
+        fieldValue = imageData;
+
+        fieldData.fieldValue = fieldValue;
+
+        this.editField.emit(fieldData);
+      }
+    } else {
+      if (!fieldPropertyValue) {
+        fieldValue = event.target.value;
+      }
+
+      fieldData.fieldValue = fieldValue;
+
+      this.editField.emit(fieldData);
+    }
+  }
+
+  updateFieldUnit(fieldPropertyName: string, fieldUnit: string, fieldIndex: number): void {
+    const fieldData: { fieldId: number, fieldPropertyName: string, fieldUnit: string, fieldIndex: number } = {
+      fieldId: this.currentFieldData!.id,
+      fieldPropertyName,
+      fieldUnit,
+      fieldIndex
+    };
+    
+    fieldData.fieldId = this.currentFieldData!.id;
+
+    this.editFieldUnit.emit(fieldData);
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
