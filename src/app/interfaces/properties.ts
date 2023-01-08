3807f6c57d7fdd842fc4f5b41f19bd1d1248c375
VelizarStavrev <velizar.g.stavrev@gmail.com>
2023-01-09 00:03:13 +0200

    Adding the properties interface.

---
+++ src/app/interfaces/properties.ts
@@ -0,0 +1,11 @@
+export interface Properties {
+    field_id: string, 
+    label: string, 
+    name: string, 
+    orderNum: string, 
+    type: string, 
+    value: string, 
+    unit: string, 
+    units: [],
+    options: []
+}
