872ebeba8cb7443cea51d7c88c5d94ecfa5a4e45
VelizarStavrev <velizar.g.stavrev@gmail.com>
2023-01-09 00:03:34 +0200

    Updating the template interface.

--- src/app/interfaces/template.ts
+++ src/app/interfaces/template.ts
@@ -2,9 +2,11 @@ export interface Template {
     data: {
         id: string,
         name: string, 
-        created: string, 
-        edited: string, 
-        notes: string
+        created: number, 
+        edited: number, 
+        notes: string,
+        fields: any[],
+        orientation: string
     },
     message: string,
     status: boolean
