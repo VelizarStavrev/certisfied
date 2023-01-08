6c64c6f020ef14bbb9f74b5db01f31fb805814ec
VelizarStavrev <velizar.g.stavrev@gmail.com>
2023-01-08 23:45:38 +0200

    Updating the template service to have a get, create, edit and delete request.

--- src/app/services/template.service.ts
+++ src/app/services/template.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { variables } from '../variables';
 import { UserService } from './user.service';
 import { Templates } from '../interfaces/templates';
+import { Template} from '../interfaces/template';
 
 @Injectable({
   providedIn: 'root'
@@ -16,6 +17,52 @@ export class TemplateService {
 
   getTemplates() {
     return this.http.get<Templates>(this.serverUrl + '/templates', 
+      { headers: {
+        'Authorization': 'Bearer ' + this.userToken
+      }});
+  }
+
+  getTemplate(templateId: string) {
+    return this.http.get<Template>(this.serverUrl + '/template/' + templateId, 
+      { headers: {
+        'Authorization': 'Bearer ' + this.userToken
+      }});
+  }
+
+  createTemplate(
+    fieldData: {
+      name: string,
+      notes: string,
+      orientation: string,
+      fields: []
+    }
+  ) {
+    return this.http.post<Template>(this.serverUrl + '/template/new', 
+      fieldData, // Body
+      { headers: {
+        'Authorization': 'Bearer ' + this.userToken
+      }});
+  }
+
+  editTemplate(
+    templateId: string, 
+    fieldData: {
+      name: string,
+      notes: string,
+      orientation: string,
+      fields: []
+    }
+  ) {
+    return this.http.post<Template>(this.serverUrl + '/template/edit/' + templateId, 
+      fieldData, // Body
+      { headers: {
+        'Authorization': 'Bearer ' + this.userToken
+      }});
+  }
+
+  deleteTemplate(templateId: string) {
+    return this.http.post<Template>(this.serverUrl + '/template/delete/' + templateId, 
+      null,
       { headers: {
         'Authorization': 'Bearer ' + this.userToken
       }});
