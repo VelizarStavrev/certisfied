053eca87ffa69ef71439a9dd1ac82fe461eaf493
VelizarStavrev <velizar.g.stavrev@gmail.com>
2023-01-17 23:43:51 +0200

    Updating the certificate service.

--- src/app/services/certificate.service.ts
+++ src/app/services/certificate.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { variables } from '../variables';
 import { UserService } from './user.service';
 import { Certificates } from '../interfaces/certificates';
+import { Certificate } from '../interfaces/certificate';
 
 @Injectable({
   providedIn: 'root'
@@ -16,6 +17,37 @@ export class CertificateService {
 
   getCertificates() {
     return this.http.get<Certificates>(this.serverUrl + '/certificates', 
+      { headers: {
+        'Authorization': 'Bearer ' + this.userToken
+      }});
+  }
+
+  getCertificate(certificateId: string) {
+    return this.http.get<Certificate>(this.serverUrl + '/certificate/' + certificateId, 
+      { headers: {
+        'Authorization': 'Bearer ' + this.userToken
+      }});
+  }
+
+  createCertificate(data: {}) {
+    return this.http.post<Certificate>(this.serverUrl + '/certificate/new', 
+      data,
+      { headers: {
+        'Authorization': 'Bearer ' + this.userToken
+      }});
+  }
+
+  editCertificate(certificateId: string, data: {}) {
+    return this.http.post<Certificate>(this.serverUrl + '/certificate/edit/' + certificateId, 
+      data,
+      { headers: {
+        'Authorization': 'Bearer ' + this.userToken
+      }});
+  }
+
+  deleteCertificate(certificateId: string) {
+    return this.http.post<Certificate>(this.serverUrl + '/certificate/delete/' + certificateId, 
+      null,
       { headers: {
         'Authorization': 'Bearer ' + this.userToken
       }});
