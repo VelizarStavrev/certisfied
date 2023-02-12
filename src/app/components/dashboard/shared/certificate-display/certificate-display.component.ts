c3e5eac7f5401eba13c67d73c0d7e441aaf7cda9
VelizarStavrev <velizar.g.stavrev@gmail.com>
2023-02-12 23:16:52 +0200

    Updating the certificate display component.

--- src/app/components/dashboard/shared/certificate-display/certificate-display.component.ts
+++ src/app/components/dashboard/shared/certificate-display/certificate-display.component.ts
@@ -1,4 +1,14 @@
-import { ChangeDetectorRef, Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
+import { 
+  ChangeDetectorRef,
+  Component,
+  ElementRef,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output,
+  ViewChild
+} from '@angular/core';
+import { variables } from 'src/app/variables';
 
 @Component({
   selector: 'app-certificate-display',
@@ -19,7 +29,7 @@ export class CertificateDisplayComponent implements OnInit {
   @Input() currentFieldListStyling: any[] = [];
   @Input() currentFieldListActive: number | null = null;
   @Output() buttonFieldClickFunc = new EventEmitter<number>();
-  @Output() updateFieldFunc = new EventEmitter<Object>();
+  @Output() updateFieldFunc = new EventEmitter<{ fieldId: number, fieldX: string, fieldY: string }>();
 
   setDimensions(): void {
     const certificateMainContainerWidth: number = this.certificateMainContainer?.nativeElement.clientWidth;
@@ -38,14 +48,14 @@ export class CertificateDisplayComponent implements OnInit {
     this.currentDragFieldDifferenceY = event.pageY - event.target.offsetTop;
   }
 
-  updateFieldPosition(event: any, fieldId: number) {
+  updateFieldPosition(event: MouseEvent, fieldId: number) {
     const fieldEndX = event.pageX;
     const fieldEndY = event.pageY;
     const containerHeight = this.certificateContainer?.nativeElement.clientHeight;
     const containerHeightOffset = this.certificateContainer?.nativeElement.offsetTop;
     const containerWidth = this.certificateContainer?.nativeElement.clientWidth;
     const containerWidthOffset = this.certificateContainer?.nativeElement.offsetLeft;
-    const pageHeaderHeight = 70; // Due to the position of the main component, the offsetTop excludes the header
+    const pageHeaderHeight = variables.headerHeight; // Due to the position of the main component, the offsetTop excludes the header
     const fieldDragOffsetX = this.currentDragFieldDifferenceX - containerWidthOffset;
     const fieldDragOffsetY = this.currentDragFieldDifferenceY - pageHeaderHeight - containerHeightOffset;
     const fieldX = ((((fieldEndX - fieldDragOffsetX) - containerWidthOffset) / containerWidth) * 100).toFixed(2);
@@ -63,7 +73,7 @@ export class CertificateDisplayComponent implements OnInit {
   ngAfterViewInit(): void {
     // Set the dimensions of the certificate
     this.setDimensions();
-    
+
     // Run change detection to not get an error due to changes
     this.cd.detectChanges();
   }
