2224ee08bdba2dbb8bdec87698e81d6ddfa4f016
VelizarStavrev <velizar.g.stavrev@gmail.com>
2023-02-17 23:11:04 +0200

    Adding tests for the loader service.

--- src/app/services/loader/loader.service.spec.ts
+++ src/app/services/loader/loader.service.spec.ts
@@ -13,4 +13,26 @@ describe('LoaderService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('#showLoader should set the isLoaderVisible value', () => {
+    expect(service.isLoaderVisible).toBeFalse();
+    
+    service.showLoader(true);
+    
+    expect(service.isLoaderVisible).toBeTrue();
+  });
+  
+  it('#showLoaderObservable should be triggered on value change', () => {
+    let expectedObservableValue = false;
+
+    service.showLoaderObservable.subscribe((value) => {
+      expect(value).toEqual(expectedObservableValue);
+    });
+
+    expectedObservableValue = true;
+    service.showLoader(true);
+
+    expectedObservableValue = false;
+    service.showLoader(false);
+  });
 });
