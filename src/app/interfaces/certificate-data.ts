be63ffa6f3269db7e076b9c794f8fa775231d7b2
VelizarStavrev <velizar.g.stavrev@gmail.com>
2023-02-19 18:39:27 +0200

    Revert "Adding the certificate data interface and implementing it in the certificate component."
    This reverts commit 571bbf3b7a5d9ffa8fdcd1bf4d22d585b058bad2.

--- src/app/interfaces/certificate-data.ts
+++
@@ -1,8 +0,0 @@
-import { FieldsInCertificate } from "./fields-in-certificate";
-
-export interface CertificateData {
-    name: string;
-    notes: string;
-    template_id: string;
-    fields: FieldsInCertificate;
-}
