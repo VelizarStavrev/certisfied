fe12ceb6d6c714a7d8c5c3e374c2a6ed2a4ebe4d
VelizarStavrev <velizar.g.stavrev@gmail.com>
2023-02-19 18:45:59 +0200

    Updating the template service and adding tests.

--- src/app/services/template/template.service.spec.ts
+++ src/app/services/template/template.service.spec.ts
@@ -1,19 +1,109 @@
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
+import { TemplateData } from 'src/app/interfaces/template-data';
 
 import { TemplateService } from './template.service';
 
 describe('TemplateService', () => {
   let service: TemplateService;
+  let http: HttpTestingController;
+  const serverURL = 'server/templates';
+  const templateId = '123456';
+  const templateData: TemplateData = {
+    id: '123',
+    name: 'Template #1',
+    notes: 'Example notes',
+    orientation: 'horizontal',
+    fields: {
+      123: {
+        id: 123,
+        template_id: '',
+        type: '',
+        properties: {
+          width: {
+            field_id: '123',
+            label: 'Width',
+            name: 'width',
+            orderNum: '1',
+            type: 'text',
+            value: 'value'
+          }
+        }
+      }
+    },
+  };
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [ HttpClientModule ]
+      imports: [
+        HttpClientTestingModule
+      ]
     });
     service = TestBed.inject(TemplateService);
+    http = TestBed.inject(HttpTestingController);
+    service.serverUrl = serverURL;
+  });
+
+  afterEach(() => {
+    http.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('#getTemplates should send a request to the server', () => {
+    service.getTemplates().subscribe(getData => {
+      const getDataReturned: any = getData;
+      expect(getDataReturned).toEqual('getTemplates response');
+    });
+
+    const req = http.expectOne(`${serverURL}/templates`);
+    expect(req.request.method).toBe('GET');
+    req.flush('getTemplates response');
+  });
+
+  it('#getTemplate should send a request to the server', () => {
+    service.getTemplate(templateId).subscribe(getData => {
+      const getDataReturned: any = getData;
+      expect(getDataReturned).toEqual(templateId);
+    });
+
+    const req = http.expectOne(`${serverURL}/template/${templateId}`);
+    expect(req.request.method).toBe('GET');
+    req.flush(templateId);
+  });
+
+  it('#createTemplate should send a request to the server', () => {
+    service.createTemplate(templateData).subscribe(postData => {
+      const postDataReturned: any = postData;
+      expect(postDataReturned).toEqual(templateData);
+    });
+
+    const req = http.expectOne(`${serverURL}/template/new`);
+    expect(req.request.method).toBe('POST');
+    req.flush(templateData);
+  });
+
+  it('#editTemplate should send a request to the server', () => {
+    service.editTemplate(templateId, templateData).subscribe(postData => {
+      const postDataReturned: any = postData;
+      expect(postDataReturned).toEqual(templateData);
+    });
+
+    const req = http.expectOne(`${serverURL}/template/edit/${templateId}`);
+    expect(req.request.method).toBe('POST');
+    req.flush(templateData);
+  });
+  
+  it('#deleteTemplate should send a request to the server', () => {
+    service.deleteTemplate(templateId).subscribe(postData => {
+      const postDataReturned: any = postData;
+      expect(postDataReturned).toEqual(templateData);
+    });
+
+    const req = http.expectOne(`${serverURL}/template/delete/${templateId}`);
+    expect(req.request.method).toBe('POST');
+    req.flush(templateData);
   });
 });
--- src/app/services/template/template.service.ts
+++ src/app/services/template/template.service.ts
@@ -2,17 +2,16 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { variables } from 'src/app/variables';
 import { Templates } from 'src/app/interfaces/templates';
-import { Template} from 'src/app/interfaces/template';
+import { Template } from 'src/app/interfaces/template';
 import { TemplateData } from 'src/app/interfaces/template-data';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TemplateService {
+  serverUrl = variables.serverURL;
 
   constructor(private http: HttpClient) { }
-
-  serverUrl = variables.serverURL;
 
   getTemplates() {
     return this.http.get<Templates>(this.serverUrl + '/templates');
@@ -22,16 +21,11 @@ export class TemplateService {
     return this.http.get<Template>(this.serverUrl + '/template/' + templateId);
   }
 
-  createTemplate(
-    fieldData: TemplateData
-  ) {
+  createTemplate(fieldData: TemplateData) {
     return this.http.post<Template>(this.serverUrl + '/template/new', fieldData);
   }
 
-  editTemplate(
-    templateId: string, 
-    fieldData: TemplateData
-  ) {
+  editTemplate(templateId: string, fieldData: TemplateData) {
     return this.http.post<Template>(this.serverUrl + '/template/edit/' + templateId, fieldData);
   }
 
