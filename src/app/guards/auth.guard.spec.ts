a3d7f178e98163b2bf501f5e646ed096601c6fe7
VelizarStavrev <velizar.g.stavrev@gmail.com>
2023-02-21 22:21:18 +0200

    Adding tests for the auth guard.

--- src/app/guards/auth.guard.spec.ts
+++ src/app/guards/auth.guard.spec.ts
@@ -1,5 +1,6 @@
 import { HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
+import { ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 
 import { AuthGuard } from './auth.guard';
 
@@ -11,9 +12,23 @@ describe('AuthGuard', () => {
       imports: [ HttpClientModule ]
     });
     guard = TestBed.inject(AuthGuard);
+    localStorage.removeItem('token');
   });
 
   it('should be created', () => {
     expect(guard).toBeTruthy();
+  });
+
+  it('#canActivate should return true if a token is present', () => {
+    const route = {} as ActivatedRouteSnapshot;
+    const state = {} as RouterStateSnapshot;
+    let canActivateReturnValue: boolean;
+
+    canActivateReturnValue = guard.canActivate(route, state) as boolean;
+    expect(canActivateReturnValue).toBeFalse();
+
+    localStorage.setItem('token', 'fakeTokenValue');
+    canActivateReturnValue = guard.canActivate(route, state) as boolean;
+    expect(canActivateReturnValue).toBeTrue();
   });
 });
